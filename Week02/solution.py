3b738ac3d8b8f5821f98b05757dbf0c786e8c971
Ristani <ristani@gmail.com>
2021-09-24 14:17:23 -0400

    Docstring adjustments, __init__ and __str__ don't need to be defined, *new_rooms specifically takes objects.

--- Week02/solution.py
+++ Week02/solution.py
@@ -3,20 +3,14 @@ class House:
     This will describe a house, capable of having any number of rooms.
     """
     def __init__(self):
-        """
-        Initialize house attributes.
-        """
         self.rooms = []
 
     def __str__(self):
-        """
-        Defines the string representation of the house object.
-        """
         return f"{type(self).__name__}:\n" + "\n".join(str(room) for room in self.rooms)
 
-    def add_rooms(self, *new_rooms):
+    def add_rooms(self, *new_rooms: object):
         """
-        Appends supplied arguments to the object rooms list.
+        Appends supplied objects to the houses list of rooms.
         """
         self.rooms += new_rooms
 
