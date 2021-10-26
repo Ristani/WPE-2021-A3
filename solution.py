dbeb14d47c19e0691cbdcd39e78c32cdf535b3d2
Ristani <ristani@gmail.com>
2021-10-25 21:28:42 -0400

    Forgot the exception.

--- solution.py
+++ solution.py
@@ -96,11 +96,11 @@ class NotEnoughSpaceError(Exception):
         message (optional): Defines the message to be displayed between the size and space outputs.
     """
 
-    def __init__(self, room_size: int, available_space: int, message="Room size exceeds available space:"):
+    def __init__(self, room_size: int, available_space: int, message: str = "Room size exceeds space:") -> None:
         self.room_size = room_size
         self.available_space = available_space
         self.message = message
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.room_size} -> {self.message} {self.available_space}"
